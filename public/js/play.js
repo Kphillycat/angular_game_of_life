be044916dca07014f43f9eb6a6e1e38d11767369
Kphillycat <keithwilliamsjr@gmail.com>
2014-03-01 19:12:07 -0500

    Add ability to play youtube video on click ofstart button

--- public/js/play.js
+++ public/js/play.js
@@ -10,19 +10,18 @@ $(document).ready(function(){
     });
   },11);
 
-  setTimeout(function(){
+  setTimeout(function(e){
     $("#start-game").on("click",function(){
       $(this).hide();
-
+      onKEEF();
     });
   },1000);
-  
   
     
 });
 
  var player;
- function onYouTubeIframeAPIReady() {
+ function onKEEF() {
   
   player = new YT.Player('player', {
     width: 420,
@@ -36,7 +35,6 @@ $(document).ready(function(){
 
   //Set up youtube
   var tag = document.createElement('script');
-
   tag.src = "https://www.youtube.com/iframe_api";
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
