76f1f3b9e6d5ac9bd12ea273db3c2545f366e0d3
Kphillycat <keithwilliamsjr@gmail.com>
2014-03-03 20:39:30 -0500

    Update API links for heroku

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -5,12 +5,11 @@ var gameOfLifeControllers = angular.module('gameOfLifeControllers',[]);
 gameOfLifeControllers.controller('gameOfLifeCtrl', ['$scope','$http',
 	function($scope, $http){
     $scope.getBoard = function(){
-      $http.get('http://localhost:9292/gol').success(function(data, status, headers, config) {
+      $http.get('/gol').success(function(data, status, headers, config) {
           $scope.board = data;
         });
     
     $scope.play = function(){ 
-          console.log("playing!");
           setInterval(function(){
             $scope.getBoard();
           }, 11);
@@ -21,13 +20,12 @@ gameOfLifeControllers.controller('gameOfLifeCtrl', ['$scope','$http',
 gameOfLifeControllers.controller('gameOfLifeForCtrl', ['$scope','$http','$routeParams',
   function($scope, $http, $routeParams){
     $scope.getBoard = function(){
-      $http.get('http://localhost:9292/gol/' + $routeParams.formation).success(function(data){
+      $http.get('/gol/' + $routeParams.formation).success(function(data){
            $scope.board = data;
         });
   }
 
    $scope.play = function(){ 
-          console.log("playing!");
           setInterval(function(){
             $scope.getBoard();
           }, 11);
